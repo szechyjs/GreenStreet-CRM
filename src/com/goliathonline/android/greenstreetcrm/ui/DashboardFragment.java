3b30a693aef1c9612e7e272391a179490d602959
Jared Szechy <jared.szechy@gmail.com>
2011-12-14 14:48:54 -0500

    initial commit

---
+++ src/com/goliathonline/android/greenstreetcrm/ui/DashboardFragment.java
@@ -0,0 +1,82 @@
+package com.goliathonline.android.greenstreetcrm.ui;
+
+import com.goliathonline.android.greenstreetcrm.R;
+import com.goliathonline.android.greenstreetcrm.util.UIUtils;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DashboardFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_dashboard, container);
+
+        // Attach event handlers
+        root.findViewById(R.id.home_btn_schedule).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if (UIUtils.isHoneycombTablet(getActivity())) {
+                    startActivity(new Intent(getActivity(), BaseMultiPaneActivity.class));
+                } else {
+                    startActivity(new Intent(getActivity(), BaseSinglePaneActivity.class));
+                }
+            }
+        });
+
+        root.findViewById(R.id.home_btn_sessions).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // Launch sessions list
+                if (UIUtils.isHoneycombTablet(getActivity())) {
+                    startActivity(new Intent(getActivity(), BaseMultiPaneActivity.class));
+                } else {
+                    startActivity(new Intent(getActivity(), BaseSinglePaneActivity.class));
+                }
+            }
+        });
+
+        root.findViewById(R.id.home_btn_starred).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // Launch list of sessions and vendors the user has starred
+                startActivity(new Intent(getActivity(), BaseSinglePaneActivity.class));                
+            }
+        });
+
+        root.findViewById(R.id.home_btn_vendors).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // Launch vendors list
+                if (UIUtils.isHoneycombTablet(getActivity())) {
+                    startActivity(new Intent(getActivity(), BaseMultiPaneActivity.class));
+                } else {
+                    startActivity(new Intent(getActivity(), BaseSinglePaneActivity.class));
+                }
+            }
+        });
+
+        root.findViewById(R.id.home_btn_map).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // Launch map of conference venue
+            	if (UIUtils.isHoneycombTablet(getActivity())) {
+                    startActivity(new Intent(getActivity(), BaseMultiPaneActivity.class));
+                } else {
+                    startActivity(new Intent(getActivity(), BaseSinglePaneActivity.class));
+                }
+            }
+        });
+
+        root.findViewById(R.id.home_btn_announcements).setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View view) {
+                        // splicing in tag streamer
+                        Intent intent = new Intent(getActivity(), BaseSinglePaneActivity.class);
+                        startActivity(intent);
+                    }
+                });
+
+        return root;
+    }
+}
---
+++ src/com/goliathonline/android/greenstreetcrm/ui/HomeActivity.java
@@ -0,0 +1,31 @@
+package com.goliathonline.android.greenstreetcrm.ui;
+
+import com.goliathonline.android.greenstreetcrm.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Front-door {@link Activity} that displays high-level features the schedule application offers to
+ * users. Depending on whether the device is a phone or an Android 3.0+ tablet, different layouts
+ * will be used. For example, on a phone, the primary content is a {@link DashboardFragment},
+ * whereas on a tablet, both a {@link DashboardFragment} and a {@link TagStreamFragment} are
+ * displayed.
+ */
+public class HomeActivity extends BaseActivity {
+	private static final String TAG = "HomeActivity";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_home);
+		getActivityHelper().setupActionBar(null, 0);
+	}
+	
+	@Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        getActivityHelper().setupHomeActivity();
+    }
+}
