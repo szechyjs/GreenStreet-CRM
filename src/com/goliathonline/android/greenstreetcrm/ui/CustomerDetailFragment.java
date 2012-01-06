832b0a928435a8af8c4748f816de644c748a1b07
Jared Szechy <jared.szechy@brilligent.com>
2012-01-06 12:29:41 -0500

    update customer detail layout, allow clicking of phones, email, and address

--- src/com/goliathonline/android/greenstreetcrm/ui/CustomerDetailFragment.java
+++ src/com/goliathonline/android/greenstreetcrm/ui/CustomerDetailFragment.java
@@ -16,6 +16,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.TextView;
@@ -35,12 +36,16 @@ public class CustomerDetailFragment extends Fragment implements
     private TextView mName;
     private CompoundButton mStarred;
 
-    private TextView mUrl;
-    private TextView mDesc;
-    private TextView mProductDesc;
+    private TextView mCompany;
+    private TextView mAddress;
+    private TextView mPhone;
+    private TextView mMobile;
+    private TextView mEmail;
     private TextView mLastChanged;
     
     private String mNameString;
+    private String mAddress1;
+    private String mAddress2;
 
     private NotifyingAsyncQueryHandler mHandler;
 
@@ -86,9 +91,11 @@ public class CustomerDetailFragment extends Fragment implements
         final View starParent = mRootView.findViewById(R.id.header_customer);
         FractionalTouchDelegate.setupDelegate(starParent, mStarred, new RectF(0.6f, 0f, 1f, 0.8f));
 
-        mUrl = (TextView) mRootView.findViewById(R.id.customer_url);
-        mDesc = (TextView) mRootView.findViewById(R.id.customer_desc);
-        mProductDesc = (TextView) mRootView.findViewById(R.id.customer_product_desc);
+        mCompany = (TextView) mRootView.findViewById(R.id.customer_company);
+        mAddress = (TextView) mRootView.findViewById(R.id.customer_address);
+        mPhone = (TextView) mRootView.findViewById(R.id.customer_phone);
+        mMobile = (TextView) mRootView.findViewById(R.id.customer_mobile);
+        mEmail = (TextView) mRootView.findViewById(R.id.customer_email);
         mLastChanged = (TextView) mRootView.findViewById(R.id.lastEdit);
 
         return mRootView;
@@ -130,10 +137,20 @@ public class CustomerDetailFragment extends Fragment implements
             mStarred.setChecked(cursor.getInt(CustomersQuery.STARRED) != 0);
             mStarred.setOnCheckedChangeListener(this);
 
-            mUrl.setText(cursor.getString(CustomersQuery.EMAIL));
-            mDesc.setText(cursor.getString(CustomersQuery.ADDRESS));
-            mProductDesc.setText(cursor.getString(CustomersQuery.CITY));
+            mCompany.setText(cursor.getString(CustomersQuery.COMPANY));
+            mAddress1 = cursor.getString(CustomersQuery.ADDRESS);
+            mAddress2 = cursor.getString(CustomersQuery.CITY) + ", " + cursor.getString(CustomersQuery.STATE) + "  " + cursor.getString(CustomersQuery.ZIP);
+            mAddress.setText(mAddress1 + "\n" + mAddress2);
+            mAddress.setOnClickListener(mAddressClick);
+            mPhone.setText("Phone: " + cursor.getString(CustomersQuery.PHONE));
+            mPhone.setOnClickListener(mPhoneClick);
+            mMobile.setText("Mobile: " + cursor.getString(CustomersQuery.MOBILE));
+            mMobile.setOnClickListener(mMobileClick);
+            mEmail.setText(cursor.getString(CustomersQuery.EMAIL));
+            mEmail.setOnClickListener(mEmailClick);
             mLastChanged.setText(UIUtils.formatTime(cursor.getLong(CustomersQuery.UPDATED), getActivity().getBaseContext()));
+            
+            
 
         } finally {
             cursor.close();
@@ -148,6 +165,38 @@ public class CustomerDetailFragment extends Fragment implements
         values.put(CustomerContract.Customers.CUSTOMER_STARRED, isChecked ? 1 : 0);
         mHandler.startUpdate(mCustomerUri, values);
     }
+    
+    private OnClickListener mPhoneClick = new OnClickListener() {
+        public void onClick(View v) {
+        	Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + mPhone.getText().subSequence(7, mPhone.getText().length())));
+    		startActivity(intent);
+        }
+    };
+    
+    private OnClickListener mMobileClick = new OnClickListener() {
+        public void onClick(View v) {
+        	Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + mMobile.getText().subSequence(8, mMobile.getText().length())));
+    		startActivity(intent);
+        }
+    };
+    
+    private OnClickListener mEmailClick = new OnClickListener() {
+        public void onClick(View v) {
+        	Intent intent = new Intent(Intent.ACTION_SEND);
+        	intent.setType("plain/text");
+        	String[] to = { mEmail.getText().toString() };
+        	intent.putExtra(Intent.EXTRA_EMAIL, to);
+    		startActivity(Intent.createChooser(intent, "Send email using:"));
+        }
+    };
+    
+    private OnClickListener mAddressClick = new OnClickListener() {
+        public void onClick(View v) {
+        	Uri mapUri = Uri.parse("geo:0,0?q=" + mAddress1 + ", " + mAddress2);
+        	Intent intent = new Intent(Intent.ACTION_VIEW, mapUri);
+    		startActivity(intent);
+        }
+    };
 
     /**
      * {@link com.goliathonline.android.greenstreetcrm.provider.CustomerContract.Customers} query parameters.
@@ -156,23 +205,29 @@ public class CustomerDetailFragment extends Fragment implements
         String[] PROJECTION = {
                 CustomerContract.Customers.CUSTOMER_LASTNAME,
                 CustomerContract.Customers.CUSTOMER_FIRSTNAME,
-                CustomerContract.Customers.CUSTOMER_CITY,
+                CustomerContract.Customers.CUSTOMER_COMPANY,
                 CustomerContract.Customers.CUSTOMER_ADDRESS,
-                CustomerContract.Customers.CUSTOMER_EMAIL,
+                CustomerContract.Customers.CUSTOMER_CITY,
+                CustomerContract.Customers.CUSTOMER_STATE,
+                CustomerContract.Customers.CUSTOMER_ZIPCODE,
                 CustomerContract.Customers.CUSTOMER_PHONE,
                 CustomerContract.Customers.CUSTOMER_MOBILE,
+                CustomerContract.Customers.CUSTOMER_EMAIL,
                 CustomerContract.Customers.CUSTOMER_STARRED,
                 SyncColumns.UPDATED,
         };
 
         int LASTNAME = 0;
         int FIRSTNAME = 1;
-        int CITY = 2;
+        int COMPANY = 2;
         int ADDRESS = 3;
-        int EMAIL = 4;
-        int PHONE = 5;
-        int MOBILE = 6;
-        int STARRED = 7;
-        int UPDATED = 8;
+        int CITY = 4;
+        int STATE = 5;
+        int ZIP = 6;
+        int PHONE = 7;
+        int MOBILE = 8;
+        int EMAIL = 9;
+        int STARRED = 10;
+        int UPDATED = 11;
     }
 }
