2b3c52c960ec5c5658dddc9457768faea5017e6b
Jared Szechy <jared.szechy@gmail.com>
2012-01-13 21:05:50 -0500

    Added a script to create changelog and add it to dropbox.

--- dropbox.bat
+++ dropbox.bat
@@ -1,1 +1,3 @@
+CALL generate-changelog.bat
 XCOPY E:\Users\Jared\workspace\GreenStreetCRM\bin\*.apk E:\Users\Jared\Dropbox\GreenStreet-CRM\ /C /D /Y
+XCOPY E:\Users\Jared\Workspace\GreenStreetCRM\changelog.txt E:\Users\Jared\Dropbox\GreenStreet-CRM\ /c /D /Y
---
+++ generate-changelog.bat
@@ -0,0 +1,1 @@
+git log --pretty=medium --abbrev-commit > changelog.txt
