6e980760291e29f6a56d6f2e039bbe39f27178e1
Jared Szechy <jared.szechy@gmail.com>
2012-01-16 00:28:23 -0500

    Add bugsense error tracking.

--- src/com/goliathonline/android/greenstreetcrm/ui/HomeActivity.java
+++ src/com/goliathonline/android/greenstreetcrm/ui/HomeActivity.java
@@ -2,6 +2,8 @@ package com.goliathonline.android.greenstreetcrm.ui;
 
 import com.goliathonline.android.greenstreetcrm.R;
 
+import com.bugsense.trace.BugSenseHandler;
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -20,6 +22,9 @@ public class HomeActivity extends BaseActivity {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.activity_home);
+		
+		BugSenseHandler.setup(this, "fd8c0e92");
+		
 		getActivityHelper().setupActionBar(null, 0);
 	}
 	
